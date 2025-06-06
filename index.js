5390618afc5772dd2369c9bc67e0371ea954f19b
Jônatas <jonatasthome04@gmail.com>
2025-06-05 22:42:25 -0300

    Applied a inquirer prompt and created create account function

--- index.js
+++ index.js
@@ -2,4 +2,33 @@ const fs = require("fs")
 const chalk = require ("chalk")
 const inquirer = require ("inquirer")
 
-console.log("Projeto inicializado com sucesso!")
+function operation(){
+    inquirer.prompt([{
+        type: "rawlist",
+        name: "action",
+        message: "O que você deseja fazer ?",
+        choices: ["Criar conta", "Consultar saldo", "Depositar", "Sacar", "Sair"]
+    },
+    {
+        type: "confirm",
+        name: "choose",
+        message: "Confirmar opção ?"
+    }
+]).then((answer)=>{
+    const action = answer.action
+    const choose = answer.choose
+    console.log(`Você escolheu a opção ${action}, aguarde um momento, você será redirecionado...`)
+    
+    if(action == "Criar conta"){
+        createAccount();
+    }
+})
+.catch((err) => console.log(err))
+}
+
+operation();
+
+function createAccount(){
+    console.log(chalk.bgGreen.black("Parabéns por escolher nosso banco"))
+    console.log(chalk.green("Defina as opções da sua conta a seguir"))
+}
