7bcf899cf37718c3ab56c3faf6a62606eb00890d
Jônatas <jonatasthome04@gmail.com>
2025-06-06 12:19:19 -0300

    Finished build account function

--- index.js
+++ index.js
@@ -51,7 +51,16 @@ function buildAccount(){
 
         if (fs.existsSync(`accounts/${accountName}.json`)){
             console.log(chalk.bgRed.black("Essa conta já existe, escolha outro nome."))
+            buildAccount();
+            return;
         }
+        
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', function(err){
+            console.log(err)
+        })
+
+        console.log(chalk.green("Parabéns, a sua conta foi criada!"))
+        operation();
     }).catch((err) =>{
 
     })
